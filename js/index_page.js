2e377fbd194d6ddf989df73d1090aca90860d236
muffyharsha <sriharsha.py@gmail.com>
2018-07-21 17:39:51 +0530

    first commit

---
+++ js/index_page.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#login-button").click(function(){
+        window.location = 'main_page.html';
+    });
+});
+
---
+++ js/main_page.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#signout").click(function(){
+        window.location = 'index.html';
+    });
+});
