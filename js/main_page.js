cd822cbf339b1ea9f025a0c9c472cfe50da3bcad
Vikky <vigvaithi@gmail.com>
2018-07-25 16:22:32 +0530

    edit bank

--- js/main_page.js
+++ js/main_page.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $("#signout").click(function(){
         window.location = 'index.html';
     });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    $('a').click(function(){
+        $("#wspace").load($(this).attr('href'));
+    });
 });
