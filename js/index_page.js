e559f5fb41b3c21bc2b0120a5b9e4cf3a5da78ef
muffyharsha <sriharsha.py@gmail.com>
2018-07-25 21:09:48 +0530

    index page form validation

--- js/index_page.js
+++ js/index_page.js
@@ -1,6 +1,48 @@
 $(document).ready(function(){
     $("#login-button").click(function(){
-        window.location = 'main_page.html';
+        var id = $("#employeeID").val();
+        var password = $("#password").val();
+        var isValid = true;
+        if (id == ""){
+            alert("Please enter Employee ID");
+            isValid = false;
+        }
+
+        if (password == ""){
+            isValid = false;
+            alert("Please enter password");
+        }
+
+        var expr_id = /^([0-9]{9})/;
+        if (id.match(expr_id) == null){
+            isValid = false;
+            alert("Please enter valid Employee id");
+        }
+
+        if (password.length < 8){
+            isValid = false;
+            alert("Please enter password with atleast 8 characters");
+        }
+
+        var expr_pass1 = /[0-9]/g;
+
+        var number_of_numbers = password.match(expr_pass1);
+        
+        if (number_of_numbers == null){
+            isValid = false;
+            alert("The password must contain atleast 2 number");
+        }
+
+        if (number_of_numbers.length < 2){
+            isValid = false;
+            alert("The password must contain atleast 2 number");
+        }
+
+
+        if (isValid){
+         window.location = 'main_page.html';
+        }
+        
     });
 });
 
