4b266772c8f244fab5e8aca175d7b4515e8b449b
Vikky <vigvaithi@gmail.com>
2018-07-26 11:39:00 +0530

    edit validation working

--- js/editBank.js
+++ js/editBank.js
@@ -3,44 +3,56 @@ $(document).ready(function(){
         var bank = $("#bank").val();
         var branch = $("#branch").val();
         var accno = $("#accno").val();
-        var scode = $("#scode").val();
+        var ifcode = $("#ifcode").val();
         
         var isValid = true;
         if (bank == ""){
             alert("Please enter bank");
             isValid = false;
         }
-
-        if (branch == ""){
-            isValid = false;
-            alert("Please enter bank branch");
-        }
-
-        if (accno == ""){
-            isValid = false;
-            alert("Please enter account number");
-        }
-
-        if (scode == ""){
-            isValid = false;
-            alert("Please enter sort code");
-        }
-
-        if (accno.length < 8){
-            isValid = false;
-            alert("Please enter a valid account number");
-        }
-
         var expr_bank =/^[a-zA-Z]*$/;
-        if (bank.match(expr_bank) == null){
+        if (isValid && bank.match(expr_bank) == null){
             isValid = false;
             alert("Please enter valid Bank");
         }
 
-        if (branch.match(expr_bank) == null){
+        if (isValid && branch == ""){
+            isValid = false;
+            alert("Please enter bank branch");
+        }
+
+        if (isValid && branch.match(expr_bank) == null){
             isValid = false;
             alert("Please enter valid Bank branch");
         }
+
+        //var expr_accno = /^[0-9]{11}$/;
+
+        if (isValid && accno == ""){
+            isValid = false;
+            alert("Please enter account number");
+        }
+
+        if (isValid && accno.length != 11){
+            isValid = false;
+            alert("Please enter valid Account Number");
+        }
+
+
+        if (isValid && ifcode == ""){
+            isValid = false;
+            alert("Please enter IFSC code");
+        }
+
+        
+
+        if(isValid && document.getElementById("check").checked == false)
+        {
+            isValid = false;
+            alert("Agree to the terms and conditions");
+        }
+
+        
         
     });
 });
